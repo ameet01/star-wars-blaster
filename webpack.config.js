1a01a130e6672e1a2dbb095e28e921f66f335809
Ameet Vadhia <ameet01@gmail.com>
2017-12-04 22:42:06 -0800

    Add working borders to area

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
           presets: ['es2015']
